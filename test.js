25f35fee4eac1e3180bb9fb0b23238638f885efe
yukiyuki2019 <yukiy0305dwycwwyhwya@gmail.com>
2020-01-28 00:06:40 +0900

    脆弱性の対策

---
+++ test.js
@@ -0,0 +1,20 @@
+'use strict';
+const pug = require('pug');
+const assert = require('assert');
+
+// pug のテンプレートにおける XSS 脆弱性のテスト
+const html = pug.renderFile('./views/posts.pug', {
+  posts: [{
+    id: 1,
+    content: '<script>alert(\'test\');</script>',
+    postedBy: 'guest1',
+    trackingCookie: '2792861693802359_6f0de698891d498bd063cbd7339516dd1282c0f5',
+    createdAt: new Date(),
+    updatedAt: new Date()
+  }],
+  user: 'guest1'
+});
+
+// スクリプトタグがエスケープされて含まれていることをチェック
+assert(html.includes('&lt;script&gt;alert(\'test\');&lt;/script&gt;'));
+console.log('テストが正常に完了しました');
